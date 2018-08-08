ec05798d0caff856886007b9170b4d8b6a02e5c0
杨思毅 <bruceyang2004@163.com>
2018-08-08 13:35:29 +0800

    not very good
    my new projects

---
+++ pizza.py
@@ -0,0 +1,7 @@
+#pizza.py
+pizzacost=eval(input("how much does each pizza cost"));
+
+pizzatotal=eval(input("how many pizza do you want"));
+totalcost=pizzacost*pizzatotal;
+print("the total cost is",totalcost)
+
